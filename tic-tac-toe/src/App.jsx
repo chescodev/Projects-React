9704935817ce510ddabcf1e5a36426bf6fa5839a
Franchesco007 <franchesco1510@gmail.com>
2024-03-30 19:06:45 -0500

    Add third project

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,14 +4,24 @@ import { Square } from './components/Square'
 import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage =window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage) 
+    return Array(9).fill(null)
+    // return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null) <-- otra forma
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+  // || <-- esto verifica si es false 
+  // ?? <-- esto verifica si es undefined o null
 
   const [winner, setWinner] = useState(null)
 
@@ -19,6 +29,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -32,6 +44,11 @@ function App() {
     // cambiar de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
     //revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
